018ffc79063f05ac668025447468f8408812abbc
aqyuki <uwaaa@aqyuki.dev>
2024-03-14 14:17:40 +0900

    feat: add css

---
+++ src/styles/font.css.ts
@@ -0,0 +1,15 @@
+import { style } from '@vanilla-extract/css'
+
+export const Jost = style({
+  fontFamily: 'Jost',
+  fontOpticalSizing: 'auto',
+  fontWeight: 400,
+  fontStyle: 'normal',
+})
+
+export const NotoSansJP = style({
+  fontFamily: 'Noto Sans JP, sans-serif',
+  fontOpticalSizing: 'auto',
+  fontWeight: 400,
+  fontStyle: 'normal',
+})
