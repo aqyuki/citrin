351cfb0d9b5a732c98c40e9a0331780e3b59fa3b
aqyuki <uwaaa@aqyuki.dev>
2024-03-14 16:13:19 +0900

    feat: add footer

---
+++ src/features/footer/footer.css.ts
@@ -0,0 +1,20 @@
+import { style } from '@vanilla-extract/css'
+
+export const footerWrapper = style({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(12,1fr)',
+  placeItems: 'left',
+  width: '100%',
+})
+
+export const content = style({
+  margin: 'auto 0',
+  fontSize: '12px',
+  color: 'gray',
+  gridColumn: '6/9',
+  '@media': {
+    '(max-width:600px)': {
+      gridColumn: '2/12',
+    },
+  },
+})
---
+++ src/features/footer/link.css.ts
@@ -0,0 +1,11 @@
+import { style } from '@vanilla-extract/css'
+
+export const linkWrapper = style({
+  display: 'flex',
+  gap: '1.5rem',
+})
+
+export const link = style({
+  fontSize: '12px',
+  color: 'gray',
+})
